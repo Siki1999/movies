847de25d2af28f98e62613723d85c8a7079fd5f8
siki1999 <matej.siketic@gmail.com>
2025-02-12 18:49:06 +0100

    Missing file commit

---
+++ script/env.js
@@ -0,0 +1,12 @@
+MOVIES_API = "5d814d103565818ffb878a9982630196"
+SUB_API = "kKj9HHgZP26NWCUmthbb1d0FJEa3LEP3"
+
+
+
+function moviesAPI() {
+    return MOVIES_API;
+} 
+
+function subAPI() {
+    return SUB_API;
+}
